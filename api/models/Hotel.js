955c5115b33e85ba86cb962005e714a3b2b841c7
3Scratch <anmolshiv1974@gmail.com>
2023-03-05 17:59:26 +0530

    Commit2:Setting up routes/initializing controllers

--- api/models/Hotel.js
+++ api/models/Hotel.js
@@ -25,12 +25,30 @@ const HotelSchema = new mongoose.Schema({
     photos:{
         type: [String],
     },
+    title:{
+        type: String,
+        required:true
+    },
     desc:{
         type: String,
         required:true
     },
     rating:{
         type: Number,
+        min:0,
+        max:5
+    },
+    rooms:{
+        type: [String],
+    },
+    cheapestPrice:{
+        type: Number,
         required:true
+    },
+    featured:{
+        type: Boolean,
+        default:false
     }
-})
+});
+
+export default mongoose.model("Hotel", HotelSchema);
